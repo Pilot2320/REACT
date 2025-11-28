f18ceb5a58383321752c9a19c80e0ccced21a075
Pilot2320 <gavrilovi0206@gmail.com>
2025-11-28 17:59:30 +0300

    kjdbfjs

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+ import { defineConfig } from 'vite'
+ import react from '@vitejs/plugin-react' 
+25 / 31
+ export default defineConfig({ 
+ plugins: [react()], 
+base: 'REACT' // добавляем эту строку 
+}) 
