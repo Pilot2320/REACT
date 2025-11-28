ab21e46e1b764a4857cfd5676081258c3cc274c4
Pilot2320 <gavrilovi0206@gmail.com>
2025-11-28 18:19:21 +0300

    efsefse

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './pages/Home';
-import Courses from './pages/Courses';
-import CourseDetail from './pages/CourseDetail';
-import Contacts from './pages/Contacts';
-import './App.css';
 
 function App() {
+  // Определяем базовый путь в зависимости от окружения
   const basePath = import.meta.env.MODE === 'production' 
-    ? '/my-courses-app'  // ← Имя вашего GitHub репозитория
+    ? '/my-courses-app'  // ← ЗАМЕНИТЕ на имя вашего репозитория!
     : '/';
 
   return (
     <Router basename={basePath}>
-      <div className="app">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/courses" element={<Courses />} />
-          <Route path="/course/:id" element={<CourseDetail />} />
-          <Route path="/contacts" element={<Contacts />} />
-        </Routes>
-      </div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/course/:id" element={<CourseDetail />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
     </Router>
   );
 }
