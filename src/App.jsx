2bf1b2c920d8afd9fe5da4b5a9ee82ca7d9c86d1
Pilot2320 <gavrilovi0206@gmail.com>
2025-11-28 19:12:07 +0300

    fsfsefs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Courses from './pages/Courses';
@@ -7,12 +7,8 @@ import Contacts from './pages/Contacts';
 import './App.css';
 
 function App() {
-  const basePath = import.meta.env.MODE === 'production' 
-    ? '/my-courses-app'
-    : '/';
-
   return (
-    <Router basename={basePath}>
+    <Router>
       <div className="app">
         <Header />
         <Routes>
